6aeaf44d9aa7c7baa268f1f436feb22d436abb84
alex.pezzini <alex.pezzini@intesys.it>
2022-04-13 14:20:31 +0200

    16 Plan/Tag | dynamize the section

--- src/reducer/tags/reducer.ts
+++ src/reducer/tags/reducer.ts
@@ -11,62 +11,62 @@ const TagsReducer = produce(
   (state: ITagsState, action: TTagsAction): ITagsState => {
     switch (action.type) {
       case "TAG_SET_LOADING":
-        state.Tag.status = LOADING_STATUS.LOADING;
-        state.Tag.operation = OPERATIONS_ACTIONS.SET;
+        state.tag.status = LOADING_STATUS.LOADING;
+        state.tag.operation = OPERATIONS_ACTIONS.SET;
         break;
       case "TAG_SET_ERROR":
-        state.Tag.status = LOADING_STATUS.ERROR;
-        state.Tag.operation = OPERATIONS_ACTIONS.SET;
+        state.tag.status = LOADING_STATUS.ERROR;
+        state.tag.operation = OPERATIONS_ACTIONS.SET;
         break;
       case "TAG_SET_SUCCESS":
-        state.Tag.status = LOADING_STATUS.SUCCESS;
-        state.Tag.operation = OPERATIONS_ACTIONS.SET;
+        state.tag.status = LOADING_STATUS.SUCCESS;
+        state.tag.operation = OPERATIONS_ACTIONS.SET;
         break;
 
       case "TAG_GET_LOADING":
-        state.Tag.status = LOADING_STATUS.LOADING;
-        state.Tag.operation = OPERATIONS_ACTIONS.GET;
+        state.tag.status = LOADING_STATUS.LOADING;
+        state.tag.operation = OPERATIONS_ACTIONS.GET;
         break;
       case "TAG_GET_ERROR":
-        state.Tag.status = LOADING_STATUS.ERROR;
-        state.Tag.operation = OPERATIONS_ACTIONS.GET;
+        state.tag.status = LOADING_STATUS.ERROR;
+        state.tag.operation = OPERATIONS_ACTIONS.GET;
         break;
       case "TAG_GET_SUCCESS":
-        state.Tag.status = LOADING_STATUS.SUCCESS;
-        state.Tag.operation = OPERATIONS_ACTIONS.GET;
-        state.Tag.item = action.payload;
+        state.tag.status = LOADING_STATUS.SUCCESS;
+        state.tag.operation = OPERATIONS_ACTIONS.GET;
+        state.tag.item = action.payload;
         break;
 
       case "TAG_DELETE_LOADING":
-        state.Tag.status = LOADING_STATUS.LOADING;
-        state.Tag.operation = OPERATIONS_ACTIONS.DELETE;
+        state.tag.status = LOADING_STATUS.LOADING;
+        state.tag.operation = OPERATIONS_ACTIONS.DELETE;
         break;
       case "TAG_DELETE_ERROR":
-        state.Tag.status = LOADING_STATUS.ERROR;
-        state.Tag.operation = OPERATIONS_ACTIONS.DELETE;
+        state.tag.status = LOADING_STATUS.ERROR;
+        state.tag.operation = OPERATIONS_ACTIONS.DELETE;
         break;
       case "TAG_DELETE_SUCCESS":
-        state.Tag.status = LOADING_STATUS.SUCCESS;
-        state.Tag.operation = OPERATIONS_ACTIONS.DELETE;
+        state.tag.status = LOADING_STATUS.SUCCESS;
+        state.tag.operation = OPERATIONS_ACTIONS.DELETE;
         break;
 
       case "TAGS_GET_LOADING":
-        state.Tags.status = LOADING_STATUS.LOADING;
-        state.Tags.operation = OPERATIONS_ACTIONS.GET;
+        state.tags.status = LOADING_STATUS.LOADING;
+        state.tags.operation = OPERATIONS_ACTIONS.GET;
         break;
       case "TAGS_GET_ERROR":
-        state.Tags.status = LOADING_STATUS.ERROR;
-        state.Tags.operation = OPERATIONS_ACTIONS.GET;
+        state.tags.status = LOADING_STATUS.ERROR;
+        state.tags.operation = OPERATIONS_ACTIONS.GET;
         break;
       case "TAGS_GET_SUCCESS":
-        state.Tags.status = LOADING_STATUS.SUCCESS;
-        state.Tags.operation = OPERATIONS_ACTIONS.GET;
-        state.Tags.items = action.payload;
+        state.tags.status = LOADING_STATUS.SUCCESS;
+        state.tags.operation = OPERATIONS_ACTIONS.GET;
+        state.tags.items = action.payload;
         break;
 
       case "TAG_RESET":
-        state.Tag.item = undefined;
-        state.Tag.status = LOADING_STATUS.INIT;
+        state.tag.item = undefined;
+        state.tag.status = LOADING_STATUS.INIT;
         state.action.type = ENTITIES_ACTIONS.IDLE;
         break;
 
