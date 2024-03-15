03628b47fe2748fa5239625873b91c28c5642fd4
SteveGT96 <steve.tsala@intesys.it>
2024-03-15 10:46:49 +0100

    feature: Implement test cases list sidebar

--- src/components/layout/SideBar/Menu/Menu.tsx
+++ src/components/layout/SideBar/Menu/Menu.tsx
@@ -5,6 +5,7 @@ import { useProject } from "../../../../lib/operators/useProject";
 import { StatusIcon } from "../../../statusIcon/StatusIcon";
 import { Trail } from "../../../trail/Trail";
 import classes from "./styles.module.scss";
+import { TestCasesList } from "../../../testCasesList";
 
 export const Menu: React.FC<{ activeCaseId: string; activeTestId: string }> = ({
   activeCaseId,
@@ -19,50 +20,11 @@ export const Menu: React.FC<{ activeCaseId: string; activeTestId: string }> = ({
       miw={300}
       listStyleType="none"
     >
-      {project.data?.testCases.map((testCase) => (
-        <List.Item key={testCase.id}>
-          <Anchor
-            component={Link}
-            underline="never"
-            to={`/project/${project.data?.id}/testCase/${testCase.id}`}
-          >
-            <Flex gap={10} key={testCase.id} align={"center"}>
-              <StatusIcon status={testCase.status} />
-              <Text
-                size="12px"
-                c={activeCaseId === testCase.id ? "primary" : "black"}
-                fw={activeCaseId === testCase.id ? "bold" : "normal"}
-              >
-                {testCase.title}
-              </Text>
-            </Flex>
-          </Anchor>
-          <List listStyleType="none">
-            {testCase.tests.map((test, index, arr) => (
-              <List.Item key={test.id} style={{ marginBottom: -7 }}>
-                <Anchor
-                  component={Link}
-                  underline="never"
-                  to={`/project/${project.data.id}/testCase/${testCase.id}/test/${test.id}`}
-                >
-                  <Flex gap={3} key={test.id} align={"center"}>
-                    <Trail isLast={arr.length === index + 1} />
-                    <StatusIcon status={test.status} />
-                    <Text
-                      ml={7}
-                      size="12px"
-                      c={activeTestId === test.id ? "primary" : "black"}
-                      fw={activeTestId === test.id ? "bold" : "normal"}
-                    >
-                      {test.title}
-                    </Text>
-                  </Flex>
-                </Anchor>
-              </List.Item>
-            ))}
-          </List>
-        </List.Item>
-      ))}
+      <TestCasesList
+        data={project.data?.testCases ?? []}
+        activeCaseId={activeCaseId}
+        activeTestId={activeTestId}
+      />
     </List>
   );
 };
