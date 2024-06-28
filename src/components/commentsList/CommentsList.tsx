cff971a7bb544eb3f3cf186f8583b51ed254e210
Nicola <nicola.bosco87@gmail.com>
2024-06-28 11:34:08 +0200

    90 | use toSorted, set ES2023

--- src/components/commentsList/CommentsList.tsx
+++ src/components/commentsList/CommentsList.tsx
@@ -152,9 +152,7 @@ export function CommentsList({
   // Sort comments
   const computedComments = useMemo(
     () =>
-      [...filteredComments].sort((a, b) =>
-        a.createdAt > b.createdAt ? -1 : 1,
-      ),
+      filteredComments.toSorted((a, b) => (a.createdAt > b.createdAt ? -1 : 1)),
     [filteredComments],
   );
 
