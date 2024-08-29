8b267bb42fdc64ce19cb10fff6ee3eefb4ec25c5
Nicola <nicola.bosco87@gmail.com>
2024-08-29 12:22:14 +0200

    101 | add remove project

--- src/components/repositories/ProjectList.tsx
+++ src/components/repositories/ProjectList.tsx
@@ -1,5 +1,5 @@
 import { useDocument } from "@automerge/automerge-repo-react-hooks";
-import { Card, Grid, Stack, Text, Title } from "@mantine/core";
+import { Card, Grid, Image, Stack, Text, Title } from "@mantine/core";
 import { useNavigate } from "react-router";
 import { parseTimestamp } from "../../lib/date/parseTimestamp";
 import { TDocType } from "../../types/schema";
@@ -7,6 +7,8 @@ import { YtServer } from "../serversContext/types";
 import classes from "./repositories.module.css";
 import { routesHelper } from "../../lib/helpers/routesHelper";
 import { AnyDocumentId } from "@automerge/automerge-repo";
+import CircleXWhite from "../../assets/icons/circle_x white.svg";
+import { openDeleteConfirmModal } from "../modals/modals";
 
 type ProjectListProps = {
   repo: YtServer;
@@ -14,42 +16,72 @@ type ProjectListProps = {
 };
 
 export const ProjectList = ({ repo, repositoryId }: ProjectListProps) => {
-  const [doc] = useDocument<TDocType>(repositoryId as AnyDocumentId);
+  const [doc, docChange] = useDocument<TDocType>(repositoryId as AnyDocumentId);
 
   const navigate = useNavigate();
 
   return (
     <>
-      {doc?.projects.map((p) => (
-        <Grid.Col span={{ base: 12, sm: 6, md: 4, lg: 3 }} key={p.id}>
-          <Card
-            className={classes.projectCard}
-            p={20}
-            radius={15}
-            c="white"
-            mih={228}
-            display="flex"
-            onClick={() => navigate(routesHelper.projectDetail(repo.id, p.id))}
-          >
-            <Stack gap={0} flex={1}>
-              <Text size="lg" fw={500} mb={5}>
-                {p.customer}
-              </Text>
-              <Title order={3} fw={700} mb={15}>
-                {p.title}
-              </Title>
-              <Text size="md" c="white" opacity={0.5} flex={1}>
-                {p.id}
-              </Text>
-              <Text>
-                Last update:
-                <br />
-                {parseTimestamp(p.lastUpdate ?? p.createdAt)}
-              </Text>
-            </Stack>
-          </Card>
-        </Grid.Col>
-      ))}
+      {doc?.projects.map((p) => {
+        const deleteProject: React.MouseEventHandler<HTMLImageElement> = (
+          e,
+        ) => {
+          e.preventDefault();
+          e.stopPropagation();
+
+          openDeleteConfirmModal("Remove project?", {
+            confirmButtonLabel: "Remove",
+            handleConfirm: () =>
+              docChange((d) => {
+                const index = d.projects.findIndex(
+                  (project) => project.id === p.id,
+                );
+                d.projects.splice(index, 1);
+              }),
+          });
+        };
+
+        return (
+          <Grid.Col span={{ base: 12, sm: 6, md: 4, lg: 3 }} key={p.id}>
+            <Card
+              className={classes.projectCard}
+              p={20}
+              radius={15}
+              c="white"
+              mih={228}
+              display="flex"
+              onClick={() =>
+                navigate(routesHelper.projectDetail(repo.id, p.id))
+              }
+            >
+              <Image
+                pos="absolute"
+                w={24}
+                top={7}
+                right={7}
+                src={CircleXWhite}
+                alt="Delete project"
+                onClick={deleteProject}
+              />
+              <Stack gap={0} flex={1}>
+                <Text size="lg" fw={500} mb={5}>
+                  {p.customer}
+                </Text>
+                <Title order={3} fw={700} mb={15}>
+                  {p.title}
+                </Title>
+                <Text size="md" c="white" opacity={0.5} flex={1}>
+                  {p.id}
+                </Text>
+                <Text>Last update:</Text>
+                <Text fw={700}>
+                  {parseTimestamp(p.lastUpdate ?? p.createdAt)}
+                </Text>
+              </Stack>
+            </Card>
+          </Grid.Col>
+        );
+      })}
     </>
   );
 };
