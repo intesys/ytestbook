76596ef6611df295becf2759de5d409281839e6a
Nicola <nicola.bosco87@gmail.com>
2024-11-20 17:16:13 +0100

    120 | handle image paste when richtextarea is open

--- src/components/shared/RichTextarea.tsx
+++ src/components/shared/RichTextarea.tsx
@@ -10,10 +10,23 @@ import Underline from "@tiptap/extension-underline";
 
 import { useEditor } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
+import Compressor from "compressorjs";
 import { Dispatch, FC, SetStateAction } from "react";
+import { convertBase64 } from "../../lib/helpers/convertBase64.ts";
 import { RichTextEditorImageControl } from "./RichTextEditorControls/RichTextEditorImageControl.tsx";
 
 export const RICHTEXTAREA_LINKEDITORDROPDOWN_CLASS = "rta-link-dropdown";
+
+const RICHTEXTAREA_ALLOWED_PASTE_IMAGE_TYPES = [
+  "image/bmp",
+  "image/gif",
+  "image/jpeg",
+  "image/jpg",
+  "image/png",
+  "image/tiff",
+  "image/webp",
+  "image/x-icon",
+];
 
 export const RichTextarea: FC<{
   label?: string;
@@ -40,6 +53,49 @@ export const RichTextarea: FC<{
     onUpdate: ({ editor }) => {
       const html = editor.getHTML();
       onChange(html);
+    },
+    enablePasteRules: false,
+  });
+
+  editor?.setOptions({
+    editorProps: {
+      handlePaste: (_view, event) => {
+        const applyImagePast = async () => {
+          const clipboardItems = await navigator.clipboard.read();
+          const clipboardItem = clipboardItems[0];
+          const { types } = clipboardItem;
+          const type = types[0];
+          const blob = await clipboardItems[0].getType(type);
+
+          // Check if paste file type is allowed (is Image)
+          if (!RICHTEXTAREA_ALLOWED_PASTE_IMAGE_TYPES.includes(type)) {
+            return;
+          }
+
+          // Compress and base64 the image
+          new Compressor(blob, {
+            quality: 0.6,
+            maxWidth: 600,
+            maxHeight: 400,
+            success: async (result) => {
+              const base64 = (await convertBase64(result)) as string;
+              editor.chain().focus().setImage({ src: base64 }).run();
+            },
+            error(err) {
+              console.log(err.message);
+            },
+          });
+        };
+
+        // If paste has files try to import the image
+        if (
+          event.clipboardData?.files.length &&
+          event.clipboardData?.files.length > 0
+        ) {
+          applyImagePast();
+          return true; // block default pasting
+        }
+      },
     },
   });
 
