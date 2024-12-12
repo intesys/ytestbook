835ef14c7d2d093c92c947358abaa51a73bc0a65
Luca Maroni <luca.maroni@intesys.it>
2024-12-12 11:22:07 +0100

    docs(addTuples, removeTuples): Add jsDoc to addTuples and removeTuples helpers

--- src/lib/helpers/addTuples.ts
+++ src/lib/helpers/addTuples.ts
@@ -1,3 +1,11 @@
+/**
+ * Adds new tuples to the target array by combining `firstIdArr` elements with the given `secondId`,
+ * ensuring no duplicate combinations already exist in the target.
+ *
+ * @param {Array<[string, string]>} target - The array of tuples where the new tuples will be added.
+ * @param {string} secondId - The second element of each tuple to be added.
+ * @param {Array<string>} firstIdArr - An array of strings to be combined with `secondId` for new tuples.
+ */
 export function addTuples(
   target: [string, string][],
   secondId: string,
--- src/lib/helpers/removeTuples.ts
+++ src/lib/helpers/removeTuples.ts
@@ -1,3 +1,10 @@
+/**
+ * Removes tuples from the target array based on a predicate function.
+ *
+ * @param {Array<[string, string]>} target - The array of tuples to remove elements from.
+ * @param {(tuple: [string, string]) => boolean} predicate - A function that determines which tuples to remove.
+ * Tuples for which the predicate returns `true` will be removed.
+ */
 export function removeTuples(
   target: [string, string][],
   predicate: (tuple: [string, string]) => boolean,
