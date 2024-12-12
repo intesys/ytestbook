e0c52ab81de7deb84222f230b1122f2736f9b110
Luca Maroni <luca.maroni@intesys.it>
2024-12-12 18:01:32 +0100

    feat(computeStatus): Add Fail to computeStatus. Refactor

--- src/lib/helpers/computeStatus.ts
+++ src/lib/helpers/computeStatus.ts
@@ -1,15 +1,20 @@
 import { StatusEnum, TCase, TStep, TTest } from "../../types/schema";
 
+// Consider DONE all tasks in the following status
+const DONE_STATUS = [StatusEnum.DONE, StatusEnum.CANCELLED];
+
 export function computeStatus(
   target: TTest | TCase,
   items: TStep[] | TTest[] | TCase[],
 ) {
-  const isDone = items.every(
-    (item) =>
-      item.status === StatusEnum.DONE || item.status === StatusEnum.CANCELLED,
-  );
+  const isFail = items.some((item) => item.status === StatusEnum.FAIL);
+  const isDone = items.every((item) => DONE_STATUS.includes(item.status));
 
-  isDone
-    ? (target.status = StatusEnum.DONE)
-    : (target.status = StatusEnum.PENDING);
+  if (isFail) {
+    target.status = StatusEnum.FAIL;
+  } else if (isDone) {
+    target.status = StatusEnum.DONE;
+  } else {
+    target.status = StatusEnum.PENDING;
+  }
 }
