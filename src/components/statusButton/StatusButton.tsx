5e42f50027258d078e4db86841debe55bff4e4cf
Nicola <nicola.bosco87@gmail.com>
2025-03-26 10:45:16 +0100

    128: show step label on step buttons

--- src/components/statusButton/StatusButton.tsx
+++ src/components/statusButton/StatusButton.tsx
@@ -1,6 +1,7 @@
 import { TUseTest } from "../../lib/operators/types";
 import { TStep } from "../../types/schema";
 import { StatusMenu } from "../statusMenu/StatusMenu";
+import { getStatusLabel } from "../../lib/helpers/getStatusLabel";
 
 type StatusButtonProps = {
   step: TStep;
