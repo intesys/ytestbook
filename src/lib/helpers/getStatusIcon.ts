d30055c5722a09ab345a65096067bcd6d36a1674
Luca Maroni <luca.maroni@intesys.it>
2025-09-05 15:45:20 +0200

    feat(getStatusIcon): Add getStatusIcon which only returns the icon component for the status
    usage: const Icon  = getStatusIcon(status); // Then use <Icon /> where needed

---
+++ src/lib/helpers/getStatusIcon.ts
@@ -0,0 +1,40 @@
+import { IconType } from "react-icons";
+import {
+  MdCheckCircle,
+  MdDangerous,
+  MdDeleteForever,
+  MdNotStarted,
+  MdPauseCircle,
+  MdPending,
+  MdReportProblem,
+} from "react-icons/md";
+import { StatusEnum } from "../../types/schema.ts";
+
+/**
+ * Returns the corresponding icon component for a given status.
+ *
+ * Example:
+ * `const Icon  = getStatusIcon(status);`.
+ * Then use `<Icon />` where needed
+ *
+ * @param {StatusEnum} status - The status to get the icon for.
+ * @returns {IconType} The icon component associated with the status.
+ */
+export const getStatusIcon = (status: StatusEnum): IconType => {
+  switch (status) {
+    case StatusEnum.BLOCKED:
+      return MdDeleteForever;
+    case StatusEnum.CANCELLED:
+      return MdDangerous;
+    case StatusEnum.DONE:
+      return MdCheckCircle;
+    case StatusEnum.FAIL:
+      return MdReportProblem;
+    case StatusEnum.PAUSED:
+      return MdPauseCircle;
+    case StatusEnum.PENDING:
+      return MdPending;
+    case StatusEnum.TODO:
+      return MdNotStarted;
+  }
+};
