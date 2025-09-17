51fca8a30ea40a935301d2dcdbda19c897fc16d8
Nicola <nicola.bosco87@gmail.com>
2025-09-17 14:17:46 +0200

    refactor(ContentWrapper): Replace div with Flex component for improved layout structure

--- src/components/layout/ContentWrapper/ContentWrapper.tsx
+++ src/components/layout/ContentWrapper/ContentWrapper.tsx
@@ -1,6 +1,11 @@
 import { PropsWithChildren } from "react";
 import classes from "./ContentWrapper.module.css";
+import { Flex } from "@mantine/core";
 
 export const ContentWrapper: React.FC<PropsWithChildren> = ({ children }) => {
-  return <div className={classes.contentWrapper}>{children}</div>;
+  return (
+    <Flex dir="column" w="100%" className={classes.contentWrapper}>
+      {children}
+    </Flex>
+  );
 };
