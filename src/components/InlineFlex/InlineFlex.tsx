fe10b6a961f861dabfc604d7143eef41087117ba
Luca Maroni <luca.maroni@intesys.it>
2025-09-18 16:04:39 +0200

    fix(InlineFlex): Fix style spreading

--- src/components/InlineFlex/InlineFlex.tsx
+++ src/components/InlineFlex/InlineFlex.tsx
@@ -7,15 +7,19 @@ interface InlineFlexProps extends FlexProps {}
  * @param {InlineFlexProps} props - Props to customize the Flex component.
  * @returns {JSX.Element} The rendered inline-flex container.
  */
-export const InlineFlex = ({ children, ...flexProps }: InlineFlexProps) => (
-  <Flex
-    component="span"
-    direction="row"
-    align="center"
-    gap={6}
-    style={{ display: "inline-flex", ...flexProps.style }}
-    {...flexProps}
-  >
-    {children}
-  </Flex>
-);
+export const InlineFlex = ({ children, ...flexProps }: InlineFlexProps) => {
+  const { style, ...rest } = flexProps;
+
+  return (
+    <Flex
+      component="span"
+      direction="row"
+      align="center"
+      gap={6}
+      style={{ display: "inline-flex", ...style }}
+      {...rest}
+    >
+      {children}
+    </Flex>
+  );
+};
