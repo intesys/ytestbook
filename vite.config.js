e1024064188b6e3b25325454765c7e0748521b4c
Luca Maroni <luca.maroni@intesys.it>
2025-09-18 11:56:14 +0200

    refactor(176): Add @ shortcut. Added @ianvs/prettier-plugin-sort-imports to avoid different IDE sorting imports differently

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from "vite";
+import path from "path";
 import react from "@vitejs/plugin-react";
-import wasm from "vite-plugin-wasm";
+import { defineConfig } from "vite";
 import topLevelAwait from "vite-plugin-top-level-await";
+import wasm from "vite-plugin-wasm";
 
 export default defineConfig({
   plugins: [topLevelAwait(), wasm(), react()],
@@ -11,6 +12,8 @@ export default defineConfig({
   },
   resolve: {
     alias: {
+      "@": path.resolve(__dirname, "src"),
+
       // /esm/icons/index.mjs only exports the icons statically, so no separate chunks are created
       "@tabler/icons-react": "@tabler/icons-react/dist/esm/icons/index.mjs",
     },
