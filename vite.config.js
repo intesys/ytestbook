d5ad78586fe5a97907c2ae93a533cdceddb93da8
Nicola <nicola.bosco87@gmail.com>
2025-09-24 09:31:11 +0200

    feat: Update dependencies and configure babel-plugin-react-compiler

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,15 @@ import topLevelAwait from "vite-plugin-top-level-await";
 import wasm from "vite-plugin-wasm";
 
 export default defineConfig({
-  plugins: [topLevelAwait(), wasm(), react()],
+  plugins: [
+    topLevelAwait(),
+    wasm(),
+    react({
+      babel: {
+        plugins: ["babel-plugin-react-compiler"],
+      },
+    }),
+  ],
   worker: {
     format: "es",
     plugins: () => [wasm()],
