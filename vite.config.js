fd7a5bcd3d838ae77b487e192a7f16f209234d55
Alessandro Falezza <alessandro.falezza@intesyt.it>
2025-12-22 14:51:34 +0100

    first naif implementation

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,40 @@
-import path from "path";
 import react from "@vitejs/plugin-react";
+import path from "path";
 import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 import topLevelAwait from "vite-plugin-top-level-await";
 import wasm from "vite-plugin-wasm";
 
 export default defineConfig({
-  plugins: [topLevelAwait(), wasm(), react()],
+  plugins: [topLevelAwait(), wasm(), react(), VitePWA({
+      registerType: "autoUpdate",
+      devOptions: {
+        enabled: true
+      },
+      manifest: {
+        name: "yTestbook",
+        short_name: "yTestbook",
+        start_url: "/",
+        display: "standalone",
+        background_color: "#0f33ce",
+        theme_color: "#0f33ce",
+        orientation: "portrait-primary",
+        "icons": [
+          {
+            "src": "/pwa-192x192.png",
+            "sizes": "192x192",
+            "type": "image/png",
+            "purpose": "any"
+          },
+          {
+            "src": "/pwa-512x512.png",
+            "sizes": "512x512",
+            "type": "image/png",
+            "purpose": "any"
+          }
+        ]
+      }
+    })],
   worker: {
     format: "es",
     plugins: () => [wasm()],
